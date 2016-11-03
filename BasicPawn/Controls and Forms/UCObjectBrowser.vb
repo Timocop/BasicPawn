607cf1798ee70d6ece51a68ca813fe8e4cc98246
Timocop <Timocop@hotmail.de>
2016-11-03 23:45:13 +0100

    Added legal stuff

--- BasicPawn/Controls and Forms/UCObjectBrowser.vb
+++ BasicPawn/Controls and Forms/UCObjectBrowser.vb
@@ -1,4 +1,21 @@
-﻿Public Class UCObjectBrowser
+﻿'BasicPawn
+'Copyright(C) 2016 TheTimocop
+
+'This program Is free software: you can redistribute it And/Or modify
+'it under the terms Of the GNU General Public License As published by
+'the Free Software Foundation, either version 3 Of the License, Or
+'(at your option) any later version.
+
+'This program Is distributed In the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty Of
+'MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License For more details.
+
+'You should have received a copy Of the GNU General Public License
+'along with this program. If Not, see < http: //www.gnu.org/licenses/>.
+
+
+Public Class UCObjectBrowser
     Private g_mFormMain As FormMain
 
     Public Sub New(f As FormMain)
