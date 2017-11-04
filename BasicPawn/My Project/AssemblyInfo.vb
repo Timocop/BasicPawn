a561e4b8c72304e226c927f80f6a3763fabe3e11
Timocop <Timocop@hotmail.de>
2017-11-04 23:34:29 +0100

    New Version

--- BasicPawn/My Project/AssemblyInfo.vb
+++ BasicPawn/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.733")>
-<Assembly: AssemblyFileVersion("0.733")>
+<Assembly: AssemblyVersion("0.734")>
+<Assembly: AssemblyFileVersion("0.734")>
